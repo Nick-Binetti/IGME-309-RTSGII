ee9e9864517ea79cc809bc7a7c539778e2270ae4
Nicholas Binetti <nib1228@rit.edu>
2025-09-22 22:38:10 -0400

    Helper methods for Assignment 1 done

--- Assignments/A01-Nicholas-Binetti/A01-Nicholas-Binetti/A01-Nicholas-Binetti.cpp
+++ Assignments/A01-Nicholas-Binetti/A01-Nicholas-Binetti/A01-Nicholas-Binetti.cpp
@@ -1,11 +1,87 @@
 #include <GL/freeglut.h> //include glut for Windows
+#include <cmath>
+
+//global variables
+int width;
+int height;
+int vertNum = 30;
+
+void drawFilledCircle(float red, float green, float blue, float center_x, float center_y, float radius)
+{
+    glColor3f(red, green, blue);
+    // draw the cicle
+    glBegin(GL_POLYGON); // filled
+    for (int i = 0; i < vertNum; ++i)
+    {
+        float t = (float)i / vertNum * 2.0f * 3.14f;
+        glVertex2f(center_x + radius * cos(t), center_y + radius * sin(t));
+    }
+    glEnd();
+}
+
+void drawWireframeCircle(float red, float green, float blue, float center_x, float center_y, float radius, float lineWidth)
+{
+    glColor3f(red, green, blue);
+    // draw the cicle
+    glBegin(GL_LINE_LOOP); //wireframe
+    for (int i = 0; i < vertNum; ++i)
+    {
+        float t = (float)i / vertNum * 2.0f * 3.14f;
+        glVertex2f(center_x + radius * cos(t), center_y + radius * sin(t));
+    }
+    glEnd();
+}
+
+void reshape(int w, int h)
+{
+    width = w;
+    height = h;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, width, 0.0, height);
+    glViewport(0, 0, width, height);
+
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+        case 27:
+            exit(0);
+            break;
+        case 43:
+            vertNum++;
+            break;
+        case 45:
+            vertNum--;
+            break;
+    }
+}
+
+void update() 
+{
+    //redraws circles so that in the case of num vertices change the user will see it instantly
+    glutPostRedisplay();
+}
 
 // called when the GL context need to be rendered
 void display(void)
 {
+
     // clear the screen to white, which is the background color
     glClearColor(1.0, 1.0, 1.0, 0.0);
-    glEnd();
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    //body
+    drawFilledCircle(0.0f, 0.0f, 0.0f, width / 2, height / 2, 100.0f);
+
+    glutSwapBuffers();
+
 }
 
 int main(int argc, char* argv[])
@@ -19,10 +95,17 @@ int main(int argc, char* argv[])
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 
     //set the initial window size */
-    glutInitWindowSize(640, 480);
+    glutInitWindowSize(width,height);
 
     // create the window with a title
-    glutCreateWindow("First OpenGL Program");
+    glutCreateWindow("Panda");
+
+    glutDisplayFunc(display); 
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutIdleFunc(update);
+
+
 
     //start the glut main loop
     glutMainLoop();
