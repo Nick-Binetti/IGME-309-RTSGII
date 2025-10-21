fd1f0a3ac6bd4cb4f579a67feeef66b915daca2b
nib1228 <nib1228@rit.edu>
2025-10-21 11:58:30 -0400

    reset particle states

--- Assignments/A03-Nicholas-Binetti/A03-Nicholas-Binetti/A03-Template/ParticleSystem.cpp
+++ Assignments/A03-Nicholas-Binetti/A03-Nicholas-Binetti/A03-Template/ParticleSystem.cpp
@@ -42,14 +42,30 @@ void ParticleSystem::update(float deltaTime)
 		/***************************/
 		// Write your code below
 		// Update lifetime, velocity, position, and color.
-		// Reset particle states (positions, velocities, colors, and lifetimes) when the lifetime reaches the maxLifeTime
+		
 		lifeTimes[i] += deltaTime;
 
 		//update velocity(grav & deltaTime), position(velocities), and color(ratio of cur vals in lifeTimes to maxLifeTime)
+		// 		
 
+		// Reset particle states (positions, velocities, colors, and lifetimes) when the lifetime reaches the maxLifeTime
 		if (lifeTimes[i] > maxLifeTime)
 		{
 			//reset everything
+			positions[i] = 0.0f; // x
+			positions[i + 1] = 0.0f; // y
+			positions[i + 2] = 0.0f; // z
+
+			velocities[i] = getRandomValue(minSpeedX, maxSpeedX);
+			velocities[i + 1] = getRandomValue(minSpeedY, maxSpeedY);
+			velocities[i + 2] = getRandomValue(minSpeedZ, maxSpeedZ);
+
+			colors[i] = 0.0f; //r
+			colors[i + 1] = 0.0f; // g
+			colors[i + 2] = 1.0f; // b
+			colors[i + 3] = 1.0f; // a
+
+			lifeTimes[i] = maxLifeTime - maxLifeTime * i / numParticles;
 		}
 		
 		// Write your code above
