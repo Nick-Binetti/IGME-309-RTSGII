026bd4b965b0d01c91fc0d49225d84c2a66b5379
nib1228 <nib1228@rit.edu>
2025-10-28 12:10:51 -0400

    week 10

--- Demos/Run-Demo/Run-Demo/Run-Demo.cpp
+++ Demos/Run-Demo/Run-Demo/Run-Demo.cpp
@@ -1,4 +1,4 @@
-// Run-Demos.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Run-Demo.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
